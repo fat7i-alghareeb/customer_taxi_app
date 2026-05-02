05ea5fb5308598f6e6528234d41bc431c09d3c3e
Fat7i <fat7ighareeb@gmail.com>
2026-05-02 14:55:41 +0300

    .

--- lib/features/order/presentation/ui/widgets/sheet/order_booking_details_step_widget.dart
+++ lib/features/order/presentation/ui/widgets/sheet/order_booking_details_step_widget.dart
@@ -46,7 +46,7 @@ class OrderBookingDetailsStepWidget extends StatelessWidget {
       context: context,
       initialDate: DateTime.now(),
       firstDate: DateTime.now(),
-      lastDate: DateTime.now().add(const Duration(days: 7)),
+      lastDate: DateTime.now().add(const Duration(days: 365 * 5)),
       builder: (context, child) {
         return Theme(
           data: Theme.of(context).copyWith(
